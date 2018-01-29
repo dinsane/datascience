8ac767af506c9e474b9c4ce8c8d391fd8c3cca01
DESKTOP-Q8NT6FF\dsane <dsanekommu@gmail.com>
2018-01-28 21:48:03 -0600

    Update to load in world population data

--- World Conflict.R
+++ World Conflict.R
@@ -2,23 +2,37 @@
 #Goal is to see how peaceful today is compared to history
 
 #load in data from csv
-mydata <- read.csv(file="C:/Users/dsane/Downloads/Conflict Catalog 18 vars.csv", header=TRUE, sep=",")
+conflictdata <- read.csv(file="C:/Users/dsane/Downloads/Conflict Catalog 18 vars.csv", header=TRUE, sep=",")
 
 #check descriptive info on data set
-summary(mydata)
+summary(conflictdata)
 #look at structure of data
-str(mydata)
+str(conflictdata)
 #get variable names
-names(mydata)
+names(conflictdata)
 #look at top 6 records
-head(mydata)
-tail(mydata)
+head(conflictdata)
+tail(conflictdata)
 #load ggplot
 install.packages("ggplot2")
 library(ggplot2)
 help(package = ggplot2)
 #create graph of the data
-qplot(mydata$StartYear, mydata$TotalFatalities, mydata)
-ggplot(mydata[1], x = StartYear, y = TotalFatalities)
+qplot(conflictdata$StartYear, conflictdata$TotalFatalities, conflictdata)
+ff <- ggplot(conflictdata, aes(StartYear, TotalFatalities))
+ff+geom_jitter()
+
+#read in data for world population by year
+popdata <- read.csv(file="C:/Users/dsane/OneDrive/Documents/datascience/API_SP.POP.TOTL_DS2_en_csv_v2.csv", header=TRUE, sep=",")
+
+summary(popdata)
+
+#remove columns to subset data
+popnumbers <- subset( popdata, select = -c(1:4))
+popnumbers <- subset( popnumbers, select = -c(ncol(popnumbers)))
+
+
+colSums(data.matrix(popnumbers))
+
 
 
